7520cdeba12304929804c44cbbf8c194174e06e6
Admin <harunrashid9159@gmail.com>
2024-12-19 17:39:31 +0530

    Code running successfully Date 19/12/2024 Time 5:39 PM

--- app/src/main/java/np/com/bimalkafle/mycalculator/MainActivity.kt
+++ app/src/main/java/np/com/bimalkafle/mycalculator/MainActivity.kt
@@ -1,16 +1,25 @@
 package np.com.bimalkafle.mycalculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import np.com.bimalkafle.mycalculator.ui.theme.MyCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +27,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyCalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            var num1 by remember {
+                mutableStateOf("0")
             }
+            var num2 by remember {
+                mutableStateOf("0")
+            }
+
+            Column(){
+                TextField(value = num1, onValueChange = {
+                    num1 = it;
+                })
+                TextField(value = num2, onValueChange = {
+                    num2 = it;
+
+                })
+
+                Row {
+                    Button(onClick = {
+                        var result = num1.toInt() + num2.toInt()
+                        Toast.makeText(applicationContext, "Result is $result", Toast.LENGTH_SHORT).show()
+
+                    }) {
+                        Text(text = "Add")
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Button(onClick = {
+                        var result = num1.toInt() - num2.toInt()
+                        Toast.makeText(applicationContext, "Result is $result", Toast.LENGTH_SHORT).show()
+
+                    }) {
+                        Text(text = "Sub")
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Button(onClick = {
+                        var result = num1.toInt() * num2.toInt()
+                        Toast.makeText(applicationContext, "Result is $result", Toast.LENGTH_SHORT).show()
+
+                    }) {
+                        Text(text = "Mul")
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Button(onClick = {
+                        var result = num1.toInt() / num2.toInt()
+                        Toast.makeText(applicationContext, "Result is $result", Toast.LENGTH_SHORT).show()
+
+                    }) {
+                        Text(text = "Div")
+                    }
+
+                }
+
+
+            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCalculatorTheme {
-        Greeting("Android")
     }
 }
